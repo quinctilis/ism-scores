271a96deccbbf60a56d63d1e2d45dcf3fe37f1bd
quinctilis <131273061+quinctilis@users.noreply.github.com>
2023-04-19 16:53:33 +0200

    Adding scripts

---
+++ README.md
@@ -0,0 +1,57 @@
+# Description
+This script scrapes the ism web page to get both smi and pmi quantitative information out 
+of their respective text based reports. 
+
+## ⚠️Disclaimer
+Read carefully the code before you run it to make 
+sure you understand the implications of doing so. In the case of ismworld, this may be 
+against its [Terms of service](https://www.ismworld.org/footer/terms-of-use/), more precisely, against:
+> exploit the Service or collect any data incorporated in the Service in any automated manner through the use of bots, metaspiders, crawlers or any other automated means;
+
+This code is intended for educational purposed only and the creator is not held liable for any 
+consequences resulting from its use
+
+## Dependencies
+There should be no dependencies (external programs) to be installed. Just having python3.8+ 
+and internet connection should be enough to install the program and run it.
+
+## Installing
+You can install the necessary python packages by running 
+```shell
+pip install -r requirements.txt
+```
+To make sure everything is installed properly, run
+```shell
+python ism.py --version
+```
+
+## Running program
+Once required packages have been installed, one can safely run the script by typing:
+```shell
+python ism.py -y --pmi march
+```
+The above command should have generated a txt file named `march_pmi.txt`. Keep in mind that 
+reports are released next month, so march report will be available in mid april. Ism webpage 
+usually stores the two most recent reports, so in the example above, if the march report 
+is available, then the february report will be available too, but the january one will not.
+
+### How scores are generated
+There are some `.csv` files accompanying the main script. 
+* **servicesindustries.csv:** The list of industries that the ism uses to generate the 
+smi report. A change on this list will result in failing to match what the report says about 
+an specific industry and the rate assigned to it.
+
+* **manufacturingindustries.csv:** The list of industries that ism uses to generate the pmi 
+report. A change on this list will result in failing to match what the report says about 
+an specific industry and the rate assigned to it.
+
+* **servicestags.csv:** Here we define the categories in smi report. In order to find which 
+category the text is referring to, we need a tag in the html defined here and an offset.
+The column multiplier is the score we assign to that category, being a positive number 
+positive sentiment and negative number negative. Usually in reports they express sentiments 
+as _backlog of orders have increased for the manufacturing industry_. So we need to know 
+if an increase in backlog of orders is positive (+1.5 multiplier for example) or negative
+(a -1.5 multiplier). All the categories for any given industry are averaged up taking into 
+consideration the weight on this `multiplier` column.
+
+* **manufacturingtags.csv:** Same as `servicestags.csv` but for the pmi report.
---
+++ ism.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+
+import requests
+import pandas as pd
+from os import path 
+import sys
+import getopt
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+def print_version():
+   print ("version 0.1")
+
+def print_help():
+   print("ism.py usage")
+   print("-u, --url U:             base url")
+   print("-i, --industries I:      file path to read industries from")
+   print("-t, --tags T:            file path to read tags from")
+   print("-o, --output F:          output file to write")
+   print("-s, --sum:               sum partial results and rank totals")
+   print("-v, --version:           Shows version and exit")
+   print("-y, --accept-disclaimer: Accept the disclaimer notice prompt")
+   print("--pmi M:                 Month to apply the default pmi on")
+   print("--smi M:                 Month to apply the default smi on")
+   print("-e, --help:              Shows help and exit")
+   
+class ism():
+    def __init__(self):
+        pass
+
+    def read_csv(self, file='fields.csv', separator=','):
+       if not path.isfile(file):
+          return None
+
+       return pd.read_csv(file, sep=separator)
+
+      
+    def read_web(self, url, selenium = False):
+       try:
+          if not selenium:
+            r = requests.get(url)
+            if r.status_code != 200:
+              print('response error: '+str(r.status_code))
+              return None
+            else:
+               return r.text
+          else:
+            service = ChromeService(executable_path=ChromeDriverManager().install())
+            options = Options()
+            options.add_argument('user-agent=fake-useragent')
+            options.add_argument("--headless=new")
+            driver = webdriver.Chrome(service=service, options=options)
+            driver.get(url)
+            driver.execute_script("arguments[0].click();", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//input[@value='Agree']"))))
+            html = driver.find_element(by=By.TAG_NAME, value= "body").get_attribute('innerHTML')
+            driver.quit()
+            return html
+       except Exception as e:
+          print(str(e))
+          return None
+   
+    def find_match(self, text, tags, after, indexes = None):
+       d = {}
+       if indexes is not None and len(indexes)!=len(tags):
+         print('indexes provided differ from tags length')
+         return None
+
+       t = [(tags[i],indexes[i],after[i]) if indexes[i] != None else 
+            (tags[i],tags[i],after[i]) for i in range(len(tags))]
+       for tag in t:
+         where = text.find(tag[0])
+         if where == -1:
+            d[tag[1]]=''
+            continue
+         if tag[2]>0:
+            lines = ''.join(text[where:].splitlines()[1:tag[2]+1])
+         else:
+            lines = ''.join(text[:where+1].splitlines()[tag[2]-1:])
+         soup = BeautifulSoup(lines, 'html.parser')
+         p = soup.find_all(class_='mb-3')
+         if len(p)>0:
+            d[tag[1]] = p[1].get_text()
+         else:
+            d[tag[1]] = soup.get_text()
+       return d
+         
+    def score(self, text, industries, multiplicator, from_mark=':', to_mark='.'):
+       d = {inds:{} for inds in industries}
+       for k,v in text.items():
+          mult = multiplicator[k]
+          for industry in industries:
+             from_ = v.find(from_mark)
+             if from_ < 0:
+                d[industry][k] = 0
+                
+             else:
+                to_ = v.find(to_mark,from_)
+                if to_ > 0:
+                   relevant = v[from_:to_]
+                   if industry in relevant:
+                      d[industry][k] = mult
+                      continue
+                   to2_ = v.find(to_mark, to_+1)
+                   if to2_ < 0:
+                      d[industry][k] = 0
+                      continue
+                   relevant = v[to_:to2_]
+                   if industry in relevant:
+                      to3_ = v.find(to_mark, to2_+1)
+                      if to3_ < 0:
+                         d[industry][k] = -mult
+                         continue
+                      relevant = v[to2_:to3_]
+                      if industry in relevant:
+                         score = -mult
+                      else:
+                         score = 0
+                      d[industry][k] = score
+                      continue
+                   d[industry][k] = 0
+                   continue
+       return d
+
+def main(argv):
+    try:
+      opts,args = getopt.getopt(argv, "u:i:t:o:svhy",
+                              ["url=",  "industries=", "tags=", 
+                               "output=", "sum", "version", "help", 
+                               "accept-disclaimer","smi=", "pmi="])
+    except getopt.GetoptError:
+      print("Error when parsing arguments")
+      print_help()
+      sys.exit(2)
+    sum_ = False    
+    accept = False
+    url = 'https://www.ismworld.org/supply-management-news-and-reports/reports/ism-report-on-business/pmi/april'
+    tags = 'manufacturingtags.csv'
+    industries = 'manufacturingindustries.csv'
+    output = ''
+    for opt, arg in opts:
+      if opt in ("-h","--help"):
+         print_help()
+         sys.exit()
+      elif opt in ("-v", "--version"):
+         print_version()
+         sys.exit()
+      elif opt in ("-u", "--url"):
+         url = str(arg) 
+      elif opt in ("-i", "--industries"):
+         industries =str(arg)
+      elif opt in ("-t", "--tags"):
+         tags =str(arg)
+      elif opt in ("-o", "--output"):
+         output = str(arg)
+      elif opt in ("-s", "--sum"):
+         sum_ = True
+      elif opt in ("-y", "--accept-disclaimer"):
+         accept = True
+      elif opt in ("--pmi"):
+         url = 'https://www.ismworld.org/supply-management-news-and-reports/reports/ism-report-on-business/pmi/'+str(arg).lower()
+         tags = 'manufacturingtags.csv'
+         industries = 'manufacturingindustries.csv'
+         output = str(arg)+"_pmi.txt"
+         sum_ = True
+      elif opt in ("--smi"):
+         url = 'https://www.ismworld.org/supply-management-news-and-reports/reports/ism-report-on-business/services/'+str(arg).lower()
+         tags = 'servicestags.csv'
+         industries = 'servicesindustries.csv'
+         output = str(arg)+"_smi.txt"
+         sum_ = True
+      else:
+         print("Some argument was misspelled")
+         print_help()
+         sys.exit(2)
+    if output == '':
+       output = 'ism.csv'
+    Myism = ism()
+    tags_df = Myism.read_csv(tags)
+    if tags_df is None:
+       print("Could not get tags from "+tags)
+       sys.exit(2)
+    tags_df.set_index(tags_df.columns.values[0], inplace=True)
+
+    industries_df = Myism.read_csv(industries, separator=';')
+    if industries_df is None:
+       print("Could not get industries from "+industries)
+       sys.exit(2)
+
+    web = Myism.read_web(url, accept)
+    if web is None:
+       print("Could not read url "+url)
+       sys.exit(2)
+    
+    d = Myism.find_match(web, tags_df[tags_df.columns.values[0]].values, 
+                        after=tags_df[tags_df.columns.values[1]].values, 
+                        indexes = tags_df.index.values)
+    if d is None:
+       sys.exit(2)
+
+    mult={tags_df.index.values[i]:tags_df.iloc[i][tags_df.columns.values[2]]
+               for i in range(len(tags_df))}
+    scores = Myism.score(d, industries_df[industries_df.columns.values[0]].values, 
+                              mult)
+    df = pd.DataFrame.from_dict(scores, orient='index')
+    if sum_:
+       df = df.sum(axis=1).squeeze().sort_values()
+       df.name = 'score'
+    df.to_csv(output, sep=';')
+if __name__ == "__main__":
+    main(sys.argv[1:])
